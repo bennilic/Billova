b9cd50a6e0dc2bc9f23ecc65ffe8950eeeac5ba7
Sergiu Iordanescu <iordanescu.sergiu@gmail.com>
2025-01-07 16:47:38 +0100

    Start with frontend implementation

--- billova_app/urls.py
+++ billova_app/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from .views import homepage
+from .views import homepage, settings, login, signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home', homepage.Home_page_view.as_view(), name='home'),
     path('index', homepage.Home_page_view.as_view(), name='index'),
+    path('settings', settings.SettingsView.as_view(), name='settings'),
+    path('login', login.LoginView.as_view(), name='login'),
+    path('sigup', signup.SignupView.as_view(), name='signup'),
     path('', homepage.Home_page_view.as_view(), name='index')
 ]
