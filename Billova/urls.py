6781125c2604cd5b6aa4d480f06a4191975dea04
Andreas Drozd <andreas.drozd@stud.fh-campuswien.ac.at>
2025-01-07 14:57:40 +0100

    add templates

--- Billova/urls.py
+++ Billova/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from views.homepage import Home_page_view
+from .views.homepage import Home_page_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
