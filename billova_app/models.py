76cbaa5ce0a75f643fae754eabbf99804498cb08
Benjamin Lichtenstein <benjamin.lichtenstein@outlook.at>
2025-01-11 23:08:36 +0100

    Adds database models.

--- billova_app/models.py
+++ billova_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Expense(models.Model):
+    dateTime = models.DateTimeField(default=timezone.now)
+    price = models.DecimalField(max_digits=14, decimal_places=2) # 1 Billion with 2 decimal places
+    note = models.TextField(blank=True)
+    invoice_issuer = models.TextField(blank=True)
+    receipt_as_text = models.TextField(blank=True)
+    category = models.ForeignKey('Category', related_name='expenses', on_delete=models.CASCADE)
+    owner = models.ForeignKey('auth.User', related_name='expenses', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['dateTime']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    user_setting = models.ForeignKey('Category', related_name='categories', on_delete=models.CASCADE)
+    owner = models.ForeignKey('auth.User', related_name='categories', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['name']
+
+
+class UserSettings(models.Model):
+    # https://en.wikipedia.org/wiki/ISO_4217 - 3 letter currency code
+    currency = models.CharField(max_length=3)
+    # https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes - 2 letter language code
+    language = models.CharField(max_length=2)
+    owner = models.OneToOneField('auth.User', related_name='user_settings', on_delete=models.CASCADE)
