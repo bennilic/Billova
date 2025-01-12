939d7e909732741b8a2f3f1bace37cd868ef78a8
Benjamin Lichtenstein <benjamin.lichtenstein@outlook.at>
2025-01-12 12:30:53 +0100

    Adds urls to viewsets, but there is a reference error.

--- Billova/settings.py
+++ Billova/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'billova_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'billova_app',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,8 @@ STATIC_URL = "billova_app/static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
--- billova_app/urls.py
+++ billova_app/urls.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from billova_app import views
 from .views import homepage, settings, login, signup
 
+router = DefaultRouter()
+router.register(r'expenses', views.Expense, basename='expense')
+router.register(r'categories', views.Category, basename='category')
+router.register(r'user_settings', views.UserSettings, basename='user_settings')
+
 urlpatterns = [
+    path('api/v1/', include(router.urls)),
+]
+
+urlpatterns += [
     path('admin/', admin.site.urls),
     path('home', homepage.HomePageView.as_view(), name='home'),
     path('index', homepage.HomePageView.as_view(), name='index'),
