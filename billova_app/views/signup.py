7f4f794b854e3a7e03f3a94fd51b92822d7a5027
Andreas Drozd <andreas.drozd@stud.fh-campuswien.ac.at>
2025-01-12 17:02:11 +0100

    work on login/signup issues

--- billova_app/views/signup.py
+++ billova_app/views/signup.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.views import View
+from django import forms
 
 class SignupView(View):
     def get(self, request):
@@ -12,6 +13,7 @@ class SignupView(View):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()  # Save the new user
+            email = form.cleaned_data.get('email')
             login(request, user)  # Log the user in after signup
             return redirect('home')  # Redirect to a page (e.g., home)
         return render(request, 'signup.html', {'form': form})
