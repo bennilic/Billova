36b8466cd05704ced1e78d522754f1c14dc2587f
Andreas Drozd <45292760+dro42@users.noreply.github.com>
2025-01-12 18:12:36 +0100

    Development/login signup #10 (#39)
    * work on task 10
    
    * work on signup and login
    
    * work on navbar
    
    * adapt navbar.html
    
    * adapt layout and work on login
    
    * work on navbar.html
    
    * comment out password reset button for now
    
    * Removes email field in signup form, since the field is not available in the standard Django UserCreationForm.
    
    * work on login
    
    * work on login/signup issues
    
    * work on login/signup issues, now login via mail and username is possible
    
    * fix account overview and settings view
    
    * fix logout button and authentication view, reformatt some files
    
    ---------
    
    Co-authored-by: Benjamin Lichtenstein <benjamin.lichtenstein@outlook.at>

--- billova_app/urls.py
+++ billova_app/urls.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
-from .views import homepage, settings, login, signup
+
+from .views import homepage, settings, signup
+from .views.account import AccountOverviewView
+from .views.account import AccountSettingsView
+from .views.categories import CategoriesView
+from .views.login import CustomLoginView
+from .views.password_reset import PwResetView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homepage.HomePageView.as_view(), name='index'),
     path('home', homepage.HomePageView.as_view(), name='home'),
     path('index', homepage.HomePageView.as_view(), name='index'),
+
+    path('account/overview/', AccountOverviewView.as_view(), name='account_overview'),
+    path('account/settings/', AccountSettingsView.as_view(), name='account_settings'),
+    path('categories/', CategoriesView.as_view(), name='categories'),
     path('settings', settings.SettingsView.as_view(), name='settings'),
-    path('login', login.LoginView.as_view(), name='login'),
+    path('login/', CustomLoginView.as_view(), name='login'),  # Logout page
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_reset/', PwResetView.as_view(), name='password_reset'),
     path('sigup', signup.SignupView.as_view(), name='signup'),
-    path('', homepage.HomePageView.as_view(), name='index')
+
 ]
