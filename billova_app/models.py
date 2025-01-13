1a0023814b42e9d4807a82945432919d370661ed
Benjamin Lichtenstein <benjamin.lichtenstein@outlook.at>
2025-01-13 20:21:37 +0100

    Adds expense / category mapping.

--- billova_app/models.py
+++ billova_app/models.py
@@ -17,6 +17,7 @@ class Expense(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    expenses = models.ManyToManyField('Expense', related_name='categories')
     owner = models.ForeignKey('auth.User', related_name='categories', on_delete=models.CASCADE)
 
     class Meta:
--- billova_app/serializers.py
+++ billova_app/serializers.py
@@ -3,21 +3,30 @@ from rest_framework import serializers
 from billova_app.models import Expense, UserSettings, Category
 
 
-class ExpenseSerializer(serializers.HyperlinkedModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
-    # category = serializers.HyperlinkedRelatedField(view_name='categories', read_only=True)
-
-    class Meta:
-        model = Expense
-        fields = ['url', 'id', 'invoice_date_time', 'price', 'note', 'invoice_issuer', 'invoice_as_text', 'owner']
-
-
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Category
         fields = ['url', 'id', 'name', 'owner']
+
+
+class ExpenseSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    categories = CategorySerializer(many=True)
+
+    class Meta:
+        model = Expense
+        fields = ['url', 'id', 'invoice_date_time', 'price', 'note', 'categories', 'invoice_issuer', 'invoice_as_text',
+                  'owner']
+
+    def create(self, validated_data):
+        categories_data = validated_data.pop('categories')
+        expense = Expense.objects.create(**validated_data)
+        for category_data in categories_data:
+            category = Category.objects.get(owner=expense.owner, **category_data)
+            expense.categories.add(category)
+        return expense
 
 
 class UserSettingsSerializer(serializers.HyperlinkedModelSerializer):
