cae4562547b900bcf0d8b64fec5f5966f5e1eb7e
Sergiu Iordanescu <iordanescu.sergiu@gmail.com>
2025-01-13 20:08:16 +0100

    6-task-implement-manual-receipt-entry - show the expenses in table

--- billova_app/views/expenses.py
+++ billova_app/views/expenses.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
 
+from billova_app.models import Expense
+
 
 class ExpensesOverview(TemplateView):
     template_name = "Billova/expenses_overview.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['expenses'] = Expense.objects.select_related('owner').all()
+        return context
