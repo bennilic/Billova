68a5eaf3d6b8e52c490047e1672b379e4c28f7e2
Andreas Drozd <andreas.drozd@stud.fh-campuswien.ac.at>
2025-01-14 13:51:55 +0100

    add comments for URL.py

--- billova_app/urls.py
+++ billova_app/urls.py
@@ -1,8 +1,7 @@
-from django.contrib import admin
-from django.urls import path
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from billova_app import api_views
 from .views import homepage, settings, signup
 from .views.account import AccountOverviewView
 from .views.account import AccountSettingsView
@@ -10,8 +9,6 @@ from .views.categories import CategoriesView
 from .views.login import CustomLoginView
 from .views.logout import CustomLogoutView
 from .views.password_reset import PwResetView
-from billova_app import api_views
-from .views import homepage, settings, login, signup
 
 router = DefaultRouter()
 router.register(r'expenses', api_views.ExpenseViewSet, basename='expense')
@@ -19,37 +16,29 @@ router.register(r'categories', api_views.CategoryViewSet, basename='category')
 router.register(r'usersettings', api_views.UserSettingsViewSet, basename='usersettings')
 
 urlpatterns = [
-    
-    """
-    API
-    """
-    path('api/v1/', include(router.urls)),
+
+    # API
+    path('api/v1/', include(router.urls), ),
     path('api/auth/', include('rest_framework.urls')),
-    
-    """
-    Home
-    """
+
+    # Home
     path('', homepage.HomePageView.as_view(), name='index'),
     path('home', homepage.HomePageView.as_view(), name='home'),
     path('index', homepage.HomePageView.as_view(), name='index'),
-    
-    """
-    Account
-    """
+
+    # Account
+
     path('account/overview/', AccountOverviewView.as_view(), name='account_overview'),
     path('account/settings/', AccountSettingsView.as_view(), name='account_settings'),
-   
-    """
-    Authentication
-    """
+
+    # Authentication
+
     path('login/', CustomLoginView.as_view(), name='login'),  # Logout page
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('password_reset/', PwResetView.as_view(), name='password_reset'),
     path('sigup', signup.SignupView.as_view(), name='signup'),
-    
-    """
-    other urls
-    """
+
+    # other urls
     path('categories/', CategoriesView.as_view(), name='categories'),
     path('settings', settings.SettingsView.as_view(), name='settings'),
 
