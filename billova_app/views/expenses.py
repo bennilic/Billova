c67eaa2ea4dd280723de0695dda0f80798753f13
Sergiu Iordanescu <iordanescu.sergiu@gmail.com>
2025-01-14 21:49:39 +0100

    Bugfixing-create-delete-entry - include category in the creation modal

--- billova_app/views/expenses.py
+++ billova_app/views/expenses.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
 
-from billova_app.models import Expense
+from billova_app.models import Expense, Category
 
 
 class ExpensesOverview(TemplateView):
@@ -9,4 +9,5 @@ class ExpensesOverview(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['expenses'] = Expense.objects.select_related('owner').all()
+        context['categories'] = Category.objects.filter(owner=self.request.user)
         return context
